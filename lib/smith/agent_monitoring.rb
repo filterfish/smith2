201b247602a9cc1ba9f2c6be0669da1989f2368a
Richard Heycock <rgh@roughage.com.au>
2011-07-15 15:12:55 +1000

    Clean up the agent monitoring

--- lib/smith/agent_monitoring.rb
+++ lib/smith/agent_monitoring.rb
@@ -11,35 +11,35 @@ module Smith
     def start_monitoring
       EventMachine::add_periodic_timer(1) do
         @agent_processes.each do |agent_process|
-          if agent_process.running?
-            if agent_process.last_keep_alive
-              if agent_process.last_keep_alive > agent_process.started_at
-                if (Time.now.utc - agent_process.last_keep_alive) > 10
-                  logger.warn("#{agent_process.name} is not responding")
-                  logger.info("Agent dead: #{agent_process.name}")
-                  agent_process.no_process_running
+          if agent_process.monitor
+            logger.debug("Agent state for #{agent_process.name}: #{agent_process.state}") if @verbose
+            case agent_process.state
+            when 'running'
+              if agent_process.last_keep_alive
+                if agent_process.last_keep_alive > agent_process.started_at
+                  if (Time.now.utc - agent_process.last_keep_alive) > 10
+                    logger.fatal("Agent not responding: #{agent_process.name}")
+                    agent_process.no_process_running
+                  end
+                else
+                  logger.warn("Discarding keep_alives with timestamp before agent started: #{agent_process.started_at} > #{agent_process.last_keep_alive}")
                 end
-              else
-                logger.warn("Discarding keep_alives with timestamp before agent started: #{agent_process.started_at} > #{agent_process.last_keep_alive}")
               end
-            else
+            when 'starting'
               if (Time.now.utc - agent_process.started_at) > 10
                 logger.error("No response from agent for > 10 seconds. Agent probably didn't start")
+                agent_process.not_responding
               else
                 logger.debug("no keep alive from #{agent_process.name}")
               end
+            when 'stoping'
+              logger.info("Agent is shutting down: #{agent_process.name}")
+            when 'dead'
+              logger.info("Restarting dead agent: #{agent_process.name}")
+              Smith::Messaging.new(:start).send_message(agent_process.name)
+            when 'unknown'
+              logger.info("Agent is in an unknown state: #{agent_process.name}")
             end
-          end
-        end
-      end
-    end
-
-    def setup_agent_restart
-      EventMachine::add_periodic_timer(1) do
-        @agent_processes.each do |agent_process|
-          if agent_process.dead?
-            logger.info("Restarting agent: #{agent_process.name}")
-            Smith::Messaging.new(:start).send_message(agent_process.name)
           end
         end
       end
@@ -47,12 +47,6 @@ module Smith
 
     def verbose=(flag)
       @verbose = flag
-    end
-
-    private
-
-    def agent_process(agent_name)
-      @agent_processes.entry(agent_name)
     end
   end
 end
