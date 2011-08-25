824dbff0c3e261a5eedb581172a96377b1c10ef8
Richard Heycock <rgh@roughage.com.au>
2011-08-26 09:12:30 +1000

    Add error message is no argument is given to Command::Start

--- lib/smith/commands/start.rb
+++ lib/smith/commands/start.rb
@@ -3,11 +3,15 @@ module Smith
   module Commands
     class Start < Command
       def execute(target)
-        target.each do |agent|
-          agents[agent].name = agent
-          agents[agent].start
+        if target.empty?
+          "Start what? No agent specified."
+        else
+          target.each do |agent|
+            agents[agent].name = agent
+            agents[agent].start
+          end
+          nil
         end
-        nil
       end
     end
   end
