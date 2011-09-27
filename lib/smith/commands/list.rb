9bd684ae497e3f3772fdd338a340eb69f383603d
Richard Heycock <rgh@roughage.com.au>
2011-09-27 12:09:35 +1000

    Add the new cli handling to list. Add --all & --long options.
    The option handling is not very elegant but isn't all option hangling
    like that. Maybe not!

--- lib/smith/commands/list.rb
+++ lib/smith/commands/list.rb
@@ -3,11 +3,59 @@ module Smith
   module Commands
     class List < Command
       def execute
-        running_agents = agents.state(:running).map(&:name)
-        if running_agents.empty?
-          "No agents running."
+        if options[:all]
+          if agents.empty?
+            "No agents running."
+          else
+            if options[:long]
+              tabulate(long_format(agents), :header => "total #{agents.count}")
+            else
+              tabulate(short_format(agents))
+            end
+          end
         else
-          "Agents running: #{running_agents.sort.join(', ')}."
+          running_agents = agents.state(:running)
+          if running_agents.empty?
+            "No agents running."
+          else
+            if options[:long]
+              tabulate(long_format(running_agents), :header => "total #{running_agents.count}")
+            else
+              tabulate(short_format(running_agents))
+            end
+          end
+        end
+      end
+
+      def options_parser
+        Trollop::Parser.new do
+          banner  Command.banner('list')
+          opt     :long,                "the number of times to send the message", :short => :l
+          opt     :all,                 "show all agents in all states", :short => :a
+        end
+      end
+
+      private
+
+      def long_format(agents)
+        agents.map do |a|
+          [a.name, a.state, a.pid, (a.started_at) ? format_time(a.started_at) : '', (a.alive?) ? '' : "(agent dead)"]
+        end
+      end
+
+      def short_format(agents)
+        agents.map { |a| [a.name] }
+      end
+
+      def format_time(t)
+        (t) ? Time.at(t).strftime("%Y/%m/%d %H:%M:%S") : ''
+      end
+
+      def tabulate(a, opts={})
+        col_widths = a.transpose.map{|col| col.map{|cell| cell.to_s.length}.max}
+        header = (opts[:header]) ? "#{opts[:header]}\n" : ''
+        a.inject(header) do |acc,e|
+          acc << sprintf("#{col_widths.inject("") { |spec,w| spec << "%-#{w + 2}s"}}\n", *e)
         end
       end
     end
