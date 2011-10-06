6f3fd20b75ee036a49a60598209c10fb683cec91
Richard Heycock <rgh@roughage.com.au>
2011-10-06 21:46:38 +1100

    Fix up the command template

--- lib/smith/commands/template.rb
+++ lib/smith/commands/template.rb
@@ -1,8 +1,8 @@
 # -*- encoding: utf-8 -*-
 module Smith
   module Commands
-    class Agents < Command
-      def execute(target)
+    class Template < Command
+      def execute
       end
     end
   end
