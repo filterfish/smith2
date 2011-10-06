05f0db37078d93cd77232e763deffbb765a73ad8
Richard Heycock <rgh@roughage.com.au>
2011-10-06 18:10:43 +1100

    Change TestAgent to override the run method & not use task

--- agents/test_agent.rb
+++ agents/test_agent.rb
@@ -1,6 +1,19 @@
 # -*- encoding: utf-8 -*-
 class TestAgent < Smith::Agent
-  task(:threads => true) do |payload|
-    logger.debug payload
+
+  options :monitor => false
+
+  def run
+    listen_and_reply('agent.test') { |metadata,payload,responder| method(:search).call(metadata, payload, responder) }
+
+    acknowledge_start
+    start_keep_alive
+    logger.info("Starting #{name}:[#{$$}]")
+  end
+
+  private
+
+  def search(metadata, payload, responder)
+    responder.value("Hey! mother fucker")
   end
 end
