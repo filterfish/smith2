29cff265737e61247d86b8048f77246163ba9d0e
Richard Heycock <rgh@roughage.com.au>
2011-10-29 10:58:58 +1100

    Check to see if the agent path exists

--- lib/smith/commands/agents.rb
+++ lib/smith/commands/agents.rb
@@ -3,18 +3,24 @@ module Smith
   module Commands
     class Agents < Command
       def execute
-        # FIXME make sure that if the path doesn't exist don't blow up.
-        agent_paths = Smith.agent_paths.inject([]) do |path_acc,path|
-          path_acc.tap do |a|
-            a << path.each_child.inject([]) do |agent_acc,p|
-                   agent_acc.tap do |b|
-                     b << Extlib::Inflection.camelize(p.basename('.rb')) if p.file? && p.basename('.rb').to_s.end_with?("agent")
+        responder.value do
+          # FIXME make sure that if the path doesn't exist don't blow up.
+          agent_paths = Smith.agent_paths.inject([]) do |path_acc,path|
+            path_acc.tap do |a|
+              if path.exist?
+                a << path.each_child.inject([]) do |agent_acc,p|
+                  agent_acc.tap do |b|
+                    b << Extlib::Inflection.camelize(p.basename('.rb')) if p.file? && p.basename('.rb').to_s.end_with?("agent")
                   end
                 end
-          end.flatten
+              else
+                error_message = "Agent path doesn't exist: #{path}"
+                responder.value(error_message)
+              end.flatten
+            end
+          end
+          (agent_paths.empty?) ? "No agents available." : "Agents available: #{agent_paths.sort.join(", ")}."
         end
-
-        responder.value((agent_paths.empty?) ? "No agents available." : "Agents available: #{agent_paths.sort.join(", ")}.")
       end
     end
   end
