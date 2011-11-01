e000522fed2b53b3240c9c16a74159a2b832d727
Richard Heycock <rgh@roughage.com.au>
2011-11-01 16:56:46 +1100

    Changes for Optimism version 3

--- lib/smith/config.rb
+++ lib/smith/config.rb
@@ -3,66 +3,67 @@ module Smith
   class Config
     @@config = Optimism do
 
-      agent do
-        monitor    true
-        singleton  true
+      agent do |c|
+        c.monitor    = true
+        c.singleton  = true
       end
 
       # Options relating to agents only. Don't put agency stuff here.
-      agents do
+      agents do |c|
         # If this is a relative path then it's relative to Smith.root_path
-        paths ['/home/rgh/dev/ruby/digivizer/agents', '/home/rgh/dev/rails/digivizer-fb-extractor/lib/agent']
+        #paths ['/home/rgh/dev/ruby/digivizer-core/agents', '/home/rgh/dev/rails/digivizer-fb-extractor/lib/agents']
+        c.paths = ['agents']
       end
 
       # These directly translate to amqp options so
       # only put options that amqp understands.
       # WARNING: UNDER NO CIRCUMSTANCES CHANGE ack TO false.
-      amqp do
-        publish do
-          ack         true
+      amqp do |c|
+        c.publish do
+          c.ack  = true
         end
 
-        pop do
-          ack         true
+        c.pop do |c|
+          c.ack  = true
         end
 
-        subscribe do
-          ack         true
+        c.subscribe do |c|
+          c.ack  = true
         end
 
-        exchange do
-          durable     true
-          auto_delete true
+        exchange do |c|
+          c.durable     = true
+          c.auto_delete = true
         end
 
-        queue do
-          durable     true
-          auto_delete true
+        queue do |c|
+          c.durable     = true
+          c.auto_delete = true
         end
 
-        broker do
-          host        'localhost'
-          port        5672
-          user        'guest'
-          password    'guest'
-          vhost       '/'
+        broker do |c|
+          c.host        = 'localhost'
+          c.port        = 5672
+          c.user        = 'guest'
+          c.password    = 'guest'
+          c.vhost       = '/'
         end
       end
 
       # Only put options that eventmachine understands here.
-      eventmachine do
-        file_descriptors 1024
+      eventmachine do |c|
+        c.file_descriptors = 1024
       end
 
-      logging do
-        trace true
-        level :debug
-        default_pattern "%d [%5p] %7l - %28c:%-3L - %m\n"
-        default_date_pattern "%Y/%m/%d %H:%M:%S"
+      logging do |c|
+        c.trace = true
+        c.level = :debug
+        c.default_pattern = "%d [%5p] %7l - %28c:%-3L - %m\n"
+        c.default_date_pattern = "%Y/%m/%d %H:%M:%S"
       end
 
-      smith do
-        namespace  'smith'
+      smith do |c|
+        c.namespace  = 'smith'
       end
     end
 
