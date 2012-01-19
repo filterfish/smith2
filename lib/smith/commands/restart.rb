96dc08e7a813092c0547da7d10a0b1bbb066e14d
Richard Heycock <rgh@roughage.com.au>
2012-01-19 12:46:48 +1100

    Restart command.
    This is somewhat immature at the moment but it works. This is also an
    example of what I shall call a meta command: it is not a command itself
    but sends messages that implement one or many existing commands.

---
+++ lib/smith/commands/restart.rb
@@ -0,0 +1,39 @@
+# -*- encoding: utf-8 -*-
+
+require_relative 'common'
+
+# This is a meta-command. It doesn't implement anything it simply sends
+# messages to one or many existing commands to do the work.
+
+module Smith
+  module Commands
+    class Restart < Command
+
+      include Common
+
+      def execute
+        Messaging::Sender.new('agency.control', :auto_delete => true, :durable => false, :immediate=>true, :mandatory=>true).ready do |sender|
+          payload = ACL::Payload.new(:agency_command).content(:command => 'stop', :args => target)
+
+          sender.publish_and_receive(payload) do |r|
+            payload = ACL::Payload.new(:agency_command).content(:command => 'start', :args => target)
+            EM.add_timer(0.5) do
+              sender.publish_and_receive(payload) do |r|
+                responder.value
+              end
+            end
+          end
+        end
+      end
+
+
+      def options_parser
+        command = self.class.to_s.split(/::/).last.downcase
+        Trollop::Parser.new do
+          banner  Command.banner(command)
+          opt     :group,     "Start everything in the specified group", :type => :string, :short => :g
+        end
+      end
+    end
+  end
+end
