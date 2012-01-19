ac6608aef201e1b182351ebe85cc512fcbc06667
Richard Heycock <rgh@roughage.com.au>
2012-01-19 17:25:33 +1100

    Allow multiple messages to be pop from a queue

--- lib/smith/commands/pop_queue.rb
+++ lib/smith/commands/pop_queue.rb
@@ -8,17 +8,28 @@ module Smith
           responder.value("No queue specified. Please specify a queue.")
         when 1
           Messaging::Receiver.new(target.shift, :auto_ack => false).ready do |receiver|
+            count = 0
             callback = proc do
-              receiver.pop do |r|
-                if options[:remove]
-                  logger.info("Removing message: #{r.metadata.delivery_tag}")
-                  r.ack
-                else
-                  logger.info("Requeuing message: #{r.metadata.delivery_tag}")
-                  r.reject(:requeue => true)
+
+              work = proc do |n,iter|
+                receiver.pop do |r|
+                  if options[:remove]
+                    logger.info("Removing message: #{r.metadata.delivery_tag}")
+                    r.ack
+                  else
+                    logger.info("Requeuing message: #{r.metadata.delivery_tag}")
+                    r.reject(:requeue => true)
+                  end
+                  count += 1
                 end
-                responder.value(r.payload.to_s.strip)
+                iter.next
               end
+
+              finished = proc do
+                responder.value("#{count} messages removed from: #{receiver.queue_name}")
+              end
+
+              EM::Iterator.new(0..options[:number] - 1).each(work, finished)
             end
 
             errback = proc {responder.value(nil)}
@@ -33,7 +44,8 @@ module Smith
       def options_parser
         Trollop::Parser.new do
           banner  Command.banner('pop-queue')
-          opt     :remove, "remove the message from the queue", :short => :r
+          opt     :remove,  "remove the message from the queue", :short => :r
+          opt     :number,  "the number of messages to remove", :default =>1,  :short => :n
         end
       end
     end
