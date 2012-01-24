57dced90cfd6c64b4b8a186b917617030db1e2f7
Richard Heycock <rgh@roughage.com.au>
2012-01-24 16:16:18 +1100

    Don't pass config as an array

--- lib/smith/config.rb
+++ lib/smith/config.rb
@@ -19,7 +19,7 @@ module Smith
         l.default_pattern = '%d [%5p] %7l - %34c:%-3L - %m\\n'
         l.default_date_pattern = '%Y/%m/%d %H:%M:%S.%N'
       end
-    end._merge!(Optimism.require_file(['/etc/smith/smithrc', "#{ENV['HOME']}/.smithrc"]))
+    end._merge!(Optimism.require('/etc/smith/smithrc', "#{ENV['HOME']}/.smithrc"))
 
     # I'm sure there's a better way of doing this ...
     @@config.agency['protocol_buffer_cache_path'] = "#{@@config.agency.cache_path.to_s}#{File::SEPARATOR}pb"
