e7b7efb343e8296a35cea4e3b3a28c712510d847
David Parry <david.parry@suranyami.com>
2012-04-03 15:38:29 +1000

    Manually setting params that would happen to be set in your ~/.smithrc This is not elegant, and their should be a way to init amqp with a standard set of configs, regardless of local config.

--- spec/lib/amqp_options_spec.rb
+++ spec/lib/amqp_options_spec.rb
@@ -6,7 +6,7 @@ require 'pp'
 describe Smith::Messaging::AmqpOptions do
 
   before(:all) do
-    @options = Smith::Messaging::AmqpOptions.new
+    @options = Smith::Messaging::AmqpOptions.new(:auto_delete => true)
     @options.queue_name = "long.queue"
   end
 
@@ -17,13 +17,23 @@ describe Smith::Messaging::AmqpOptions do
   it 'should return the correct default queue options' do
     @options.queue.should == {:durable => true, :auto_delete => true}
   end
-
-  it 'should return the correct default publish options' do
-    @options.publish.should == {:ack => true, :routing_key => "long.queue"}
+  
+  describe "with publish options" do
+    before do
+      @options = Smith::Messaging::AmqpOptions.new(:ack => true)
+      @options.queue_name = "long.queue"
+    end
+    it 'should return the correct default publish options' do
+      @options.publish.should == {:ack => true, :routing_key => "long.queue", :headers => {}}
+    end
   end
-
-  it 'should return the correct default subscribe options' do
-    @options.subscribe.should == {:ack => true}
+  describe "with publish options" do
+    before do
+      @options = Smith::Messaging::AmqpOptions.new(:ack => true)
+    end
+    it 'should return the correct default subscribe options' do
+      @options.subscribe.should == {:ack => true}
+    end
   end
 
   it 'should expand and merge short options to the proper amqp options' do
