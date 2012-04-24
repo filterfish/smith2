598d927c0d8fde83dd2b197ee2c321b43cef34e7
Richard Heycock <rgh@roughage.com.au>
2012-04-24 19:12:15 +1000

    Clean up the Command class.
    I've added a new class called CommandBase that all commands inherit from
    and moved all option processing into CommandBase. This is much cleaner
    and less hacky.

--- lib/smith/command.rb
+++ lib/smith/command.rb
@@ -24,9 +24,10 @@ module Smith
       clazz = Commands.const_get(Extlib::Inflection.camelize(command)).new
 
       begin
-        options, target = parse_options(clazz, args)
 
-        vars.merge(:options => options, :target => target).each do |k,v|
+        clazz.parse_options(args)
+
+        vars.each do |k,v|
           clazz.instance_eval <<-EOM, __FILE__, __LINE__ + 1
             instance_variable_set(:"@#{k}", v)
             def #{k}=(z); @#{k} = z; end
@@ -37,28 +38,10 @@ module Smith
         clazz.execute
 
       rescue Trollop::CommandlineError => e
-        vars[:responder].value(parser_help(clazz, :prefix => "Error: #{e.message}.\n"))
+        vars[:responder].value(clazz.format_help(:prefix => "Error: #{e.message}.\n"))
       rescue Trollop::HelpNeeded
-        vars[:responder].value(parser_help(clazz))
+        vars[:responder].value(clazz.format_help)
       end
-    end
-
-    # Banner callback. This is called when the parser is called.
-    # I'm sure there is a better way of doing this.
-    def self.banner(command)
-      "smithctl #{command} OPTIONS [Agents]"
-    end
-
-    def self.banner_template(clazz)
-      return <<EOS
-
-  %s
-
-  Usage:
-    smithctl #{clazz.class.to_s.split('::').last.downcase} Options
-
-Options are:
-EOS
     end
 
     private
@@ -114,27 +97,6 @@ EOS
     # Return the command base path.
     def self.base_path
       @c64a6f4f ||= Smith.root_path.join('lib').join("smith").join('commands')
-    end
-
-    # Uses the options_parser method in the specific command class to procees
-    # any options associated with that command. If no options_parser method
-    # exits then an empty Array is returned. Any members of the args array
-    # that are not parsed by the options parser are return as the target, i.e.
-    # the agent(s) that the command is to operate on.
-    def self.parse_options(clazz, args)
-      if clazz.respond_to?(:options_parser)
-        [clazz.options_parser.parse(args), args]
-      else
-        [{}, args]
-      end
-    end
-
-    def self.parser_help(clazz, opts={})
-      StringIO.new.tap do |help|
-        help.puts opts[:prefix] if opts[:prefix]
-        clazz.options_parser.educate(help)
-        help.rewind
-      end.read
     end
   end
 end
