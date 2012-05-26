f4886b56b16c4ac72c8ffecbab04b7589d66acc8
Richard Heycock <rgh@roughage.com.au>
2012-05-26 15:54:30 +0100

    Rename messaging/exceptions.rb to exceptions.rb & add new Exception.
    Added UnknownEnvironmentError.

--- lib/smith/exceptions.rb
+++ lib/smith/exceptions.rb
@@ -1,5 +1,11 @@
 # -*- encoding: utf-8 -*-
 module Smith
+  class UnknownEnvironmentError < RuntimeError
+    def initialize(message=nil)
+      super("Invalid environment: #{message || Smith.environment}")
+    end
+  end
+
   module Messaging
     class IncompletePayload < RuntimeError; end
     class IncorrectPayloadType < RuntimeError; end
