9d8429da3845b6de8aea2332985325fcc84115a9
Richard Heycock <rgh@filterfish.org>
2012-06-28 12:18:33 +1000

    Update TestAgent to use the new reply code.

--- agents/test_agent.rb
+++ agents/test_agent.rb
@@ -6,12 +6,17 @@ class TestAgent < Smith::Agent
   def run
     n = 0
 
-    receiver('agent.fast_test') do |r|
-      acl = Smith::ACL::Payload.new(:default)
-      sender('agent.barf') do |send_queue|
-        send_queue.publish_and_receive(acl.content("#{n +=1 } hey!")) do |r|
-          puts "echoing payload: #{r.payload}"
-        end
+    sender('agent.barf') do |send_queue|
+
+      logger.debug { "Setting up reply handler." }
+
+      send_queue.on_reply do |r|
+        puts "Echoing payload: #{r.payload}"
+      end
+
+      receiver('agent.fast_test') do |queue|
+        puts "Sending message to #{send_queue.queue_name}"
+        send_queue.publish(Smith::ACL::Payload.new(:default).content("#{n +=1 } hey!"))
       end
     end
   end
