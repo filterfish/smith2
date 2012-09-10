f6f64022cbbb05928894c0df7a4e002fa862604e
Richard Heycock <rgh@filterfish.org>
2012-09-10 10:05:26 +1000

    MessageFactory.
    Instead of trying to put everything in the Payload class use a factory.
    This removes all the special cases that need to be hacked when creating
    payloads (eg nested ACLs).

--- lib/smith/messaging/payload.rb
+++ lib/smith/messaging/payload.rb
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 module Smith
-  module ACL
 
+  module ACL
     module ACLInstanceMethods
       def inspect
         "<#{self.class.to_s}> -> #{self.to_hash}"
@@ -32,6 +32,80 @@ module Smith
             end
           end
         end
+      end
+    end
+
+    class Factory
+
+      @@acl_classes = {:default => Default}
+
+      class << self
+        def create(type, content={}, &blk)
+          type = type.to_s
+
+          unless @@acl_classes.include?(type)
+            logger.debug { "Loding ACL: #{type}" }
+            # decorate the ACL class
+            @@acl_classes[type] = clazz(type).send(:include, ACLInstanceMethods)
+            @@acl_classes[type].send(:define_method, :_type) {type}
+          end
+
+          if blk
+            if content.empty?
+              @@acl_classes[type].new.tap { |m| blk.call(m) }
+            else
+              raise ArgumentError, "You cannot give a content hash and a block."
+            end
+          else
+            (content.empty?) ? @@acl_classes[type].new : @@acl_classes[type].new(content)
+          end
+        end
+
+        def clazz(type)
+          type.split(/::/).inject(ACL) do |a,m|
+            a.const_get(Extlib::Inflection.camelize(m))
+          end
+        end
+      end
+    end
+
+    class NewPayload
+      include Logger
+
+      # content can be an existing ACL class.
+      def initialize(acl, opts={})
+        @acl = acl
+      end
+
+      # The type of content.
+      def _type
+        @acl._type
+      end
+
+      # Returns a hash of the payload.
+      def to_hash
+        @acl.to_hash
+      end
+
+      # Encode the content, returning the encoded data.
+      def encode
+        @acl.serialize_to_string
+      end
+
+      # Returns true if the payload has all its required fields set.
+      def initialized?
+        raise RuntimeError, "You probably forgot to call #content or give the :from option when instantiating the object." if @acl.nil?
+        @acl.initialized?
+      end
+
+      # Convert the payload to a pretty string.
+      def to_s
+        @acl.inspect
+      end
+
+      # Decode the content using the specified decoder.
+      def self.decode(payload, type=:default)
+        Factory.create(type).parse_from_string(payload)
       end
     end
 
@@ -94,8 +168,8 @@ module Smith
       end
 
       # Decode the content using the specified decoder.
-      def self.decode(payload, decoder=:default)
-        content_class(decoder).new.parse_from_string(payload)
+      def self.decode(payload, type=:default)
+        content_class(type).new.parse_from_string(payload)
       end
     end
   end
