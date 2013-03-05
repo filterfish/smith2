36339d9b3f8a646efacd3629eeb5687550a5a729
Richard Heycock <rgh@filterfish.org>
2013-03-05 20:04:33 +1100

    Command to return the status of the agency.

---
+++ lib/smith/commands/smithctl/status.rb
@@ -0,0 +1,27 @@
+# -*- encoding: utf-8 -*-
+
+require 'smith/messaging/queue'
+
+module Smith
+  module Commands
+    class Status < CommandBase
+      def execute
+        status do |s|
+          responder.succeed((s) ? "Agency running" : "Agency not running")
+        end
+      end
+
+      def status(&blk)
+        Messaging::Queue.number_of_consumers(QueueDefinitions::Agency_control) do |consumers_count|
+          blk.call(consumers_count > 0)
+        end
+      end
+
+      private
+
+      def options_spec
+        banner "Shows the status of the agency."
+      end
+    end
+  end
+end
