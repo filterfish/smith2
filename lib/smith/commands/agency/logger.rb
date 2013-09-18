d6e2e3208350976da85484819842dfaedf7a6a00
Richard Heycock <rgh@filterfish.org>
2013-09-19 09:59:33 +1000

    Don't kill the agency when sending incorrect logger commands.

--- lib/smith/commands/agency/logger.rb
+++ lib/smith/commands/agency/logger.rb
@@ -3,39 +3,52 @@ module Smith
   module Commands
     class Logger < CommandBase
       def execute
-        responder.succeed(_logger)
+        responder.succeed((_logger))
       end
 
       def _logger(&blk)
+        error_messages = []
         if options[:level].nil?
-          "No log level. You must specify a log level and a target"
+          error_messages << "No log level. You must specify a log level and a target"
         else
           case target.first
           when 'all'
             agents.state(:running).each do |agent|
-              send_agent_control_message(agent, :command => 'log_level', :options => options[:level])
+              error_messages << set_log_level(agent.uuid)
             end
+            ''
           when 'agency'
             begin
               logger.info { "Setting agency log level to: #{options[:level]}" }
               log_level(options[:level])
             rescue ArgumentError
-              logger.error { "Incorrect log level: #{options[:level]}" }
+              m = "Incorrect log level: #{options[:level]}"
+              logger.error { m }
+              error_messages << m
             end
+            ''
           when nil
-            "No target. You must specify one of the following: 'agency', 'all' or a list of agents"
+            error_messages << "No target. You must specify one of the following: 'agency', 'all' or a list of agents"
           else
-            target.each do |agent|
-              if agents[agent].running?
-                send_agent_control_message(agents[agent], :command => 'log_level', :options => [options[:level]])
-              end
+            target.each do |uuid|
+              error_messages << set_log_level(uuid)
             end
           end
         end
-        ''
+        error_messages.compact.join(", ")
       end
 
       private
+
+      def set_log_level(uuid)
+        agent = agents[uuid]
+        if agent && agent.running?
+          send_agent_control_message(agent, :command => 'log_level', :options => [options[:level]])
+          nil
+        else
+          "Agent does not exist: #{uuid}"
+        end
+      end
 
       def send_agent_control_message(agent, message)
         Messaging::Sender.new(agent.control_queue_def) do |sender|
