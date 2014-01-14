5b48620eea39154d71b85d3288455fd238f6955d
Richard Heycock <rgh@filterfish.org>
2014-01-15 10:01:57 +1100

    Add direction flag to the firehose command & bug fixes.
    The rabbitmq trace command can track messages entering or leaving the
    broker. The "direction" is from the perspective of the client not the
    broker.
    
    ACL handling was also broken.

--- lib/smith/commands/smithctl/firehose.rb
+++ lib/smith/commands/smithctl/firehose.rb
@@ -8,10 +8,17 @@ module Smith
         AMQP::Channel.new(Smith.connection) do |channel,ok|
           channel.topic('amq.rabbitmq.trace', :durable => true) do |exchange|
             channel.queue('smith.firehose', :durable => true) do |queue|
-              queue.bind(exchange, :routing_key => "publish.#{Smith.config.smith.namespace}.#{queue_name}").subscribe do |m,p|
-                clazz = @acl_type_cache.get_by_hash(m.headers['properties']['type'])
-                message = clazz.new.parse_from_string(data)
-                puts (options[:json_given]) ? message.to_json : message.inspect
+              if correct_direction?
+                routing_key = "#{options[:direction]}.#{Smith.config.smith.namespace}.#{queue_name}"
+
+                queue.bind(exchange, :routing_key => routing_key).subscribe do |m, payload|
+                  acl_type_cache = AclTypeCache.instance
+                  clazz = acl_type_cache.get_by_hash(m.headers['properties']['type'])
+                  message = {options[:direction] => clazz.new.parse_from_string(payload)}
+                  puts (options[:json_given]) ? message.to_json : message.inspect
+                end
+              else
+                responder.succeed("--direction must be either deliver or publish")
               end
             end
           end
@@ -24,7 +31,12 @@ module Smith
              "  You _must_ run 'rabbitmqctl trace_on' for this to work."]
         banner b.join("\n")
 
-        opt    :json ,  "return the JSON representation of the message", :short => :j
+        opt    :json,       "return the JSON representation of the message", :short => :j
+        opt    :direction,  "Show messages that are leaving the broker [deliver|publish]", :short => :d, :type => :string, :default => 'deliver'
+      end
+
+      def correct_direction?
+        options[:direction] == 'deliver' || options[:direction] == 'publish'
       end
     end
   end
