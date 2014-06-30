b2e87a4965e5fe6dbf691186c3f5e4c658982d65
Richard Heycock <rgh@filterfish.org>
2014-06-30 18:39:56 +1000

    Revamp the config. Use toml.
    This is the last config format change. Promise. TOML has a fair bit of
    support so it's unlikely to go away and it's a fairly reasonable way to
    do config.

--- lib/smith/config.rb
+++ lib/smith/config.rb
@@ -1,7 +1,13 @@
 #!/usr/bin/env ruby
 # -*- encoding: utf-8 -*-
 
+require 'toml'
 require 'pathname'
+require 'hashie/extensions/coercion'
+require 'hashie/extensions/deep_merge'
+require 'hashie/extensions/method_access'
+require 'hashie/extensions/merge_initializer'
+
 
 module Smith
 
@@ -11,45 +17,12 @@ module Smith
 
     CONFIG_FILENAME = '.smithrc'
 
-    attr_accessor :agent, :agency, :amqp, :logging, :smith, :eventmachine, :smith, :ruby
-
-    to_hash = proc do
-      def to_hash
-        Hash[*members.zip(values).flatten]
-      end
-
-      def merge(h)
-        to_hash.merge(h)
-      end
-
-      def has_key?(k)
-        to_hash.has_key?(k)
-      end
-    end
-
-    Struct.new("Agent", :monitor, :singleton, :metadata, :prefetch, &to_hash)
-    Struct.new("Agency", :cache_path, :agent_path, :acl_path, :acl_cache_path, :pid_dir, &to_hash)
-    Struct.new("AmqpOpts", :durable, :auto_delete, &to_hash)
-    Struct.new("Broker", :host, :port, :user, :password, :vhost, &to_hash)
-    Struct.new("Subscribe", :ack, &to_hash)
-    Struct.new("Pop", :ack, &to_hash)
-    Struct.new("Publish", :headers, &to_hash)
-    Struct.new("Amqp", :broker, :exchange, :queue, :publish, :subscribe, :pop, &to_hash)
-    Struct.new("Appender", :type, :filename, &to_hash)
-    Struct.new("Logging", :trace, :level, :default_pattern, :default_date_pattern, :appender, :filetype, :vhost, &to_hash)
-    Struct.new("Smith", :namespace, :timeout, &to_hash)
-    Struct.new("Eventmachine", :file_descriptors, :epoll, :kqueue, &to_hash)
-
     def initialize
       load_config
     end
 
     def reload
       @config = Config.new
-    end
-
-    def to_hash
-      {:agent => @agent, :agency => @agency, :amqp => @amqp, :eventmachine => @eventmachine, :logging => @logging, :smith => @smith, :ruby => @ruby}
     end
 
     def path
@@ -60,80 +33,39 @@ module Smith
       @config ||= Config.new
     end
 
+    def method_missing(method, *args)
+      @config.send(method, *args)
+    end
+
     private
 
-    def set_as_boolean(config, k, default=nil)
-      v = config[k]
-      if v.nil? && default
-        default
-      else
-        v = v.downcase
-        if v == 'true'
-          true
-        elsif v == 'false'
-          false
-        else
-          raise ArgumentError, "#{k} must be true or false, #{v} given."
-        end
-      end
-    end
+    class ConfigHash < Hash
+      include Hashie::Extensions::Coercion
+      include Hashie::Extensions::DeepMerge
+      include Hashie::Extensions::MethodReader
+      include Hashie::Extensions::MergeInitializer
 
-    def set_as_string(config, k, default=nil)
-      config[k]
-    end
-
-    def set_as_integer(config, k, default=nil)
-      v = config[k]
-      if v.nil? && default
-        default
-      else
-        begin
-          Integer(v)
-        rescue
-          raise ArgumentError, "#{k} must be an integer. #{v} given."
-        end
-      end
+      coerce_value Hash, ConfigHash
     end
 
     def load_config
-      config = read_config_file(find_config_file)
-
-      amqp_opts = Struct::AmqpOpts.new(true, false)
-      cache_path = Pathname.new(config[:agency_cache_path]).expand_path
-      local_acl_path = Pathname.new(__FILE__).dirname.join('messaging').join('acl').expand_path
-      acl_path = "#{local_acl_path}#{File::PATH_SEPARATOR}#{config[:acl_path]}"
-      broker = Struct::Broker.new(config[:broker_host], set_as_integer(config, :broker_port), config[:broker_user], config[:broker_password], config[:broker_vhost] || '/')
-      appender = Struct::Appender.new(config[:logging_appender_type], config[:logging_appender_filename])
-
-      @agent = Struct::Agent.new(set_as_boolean(config, :agent_monitor), set_as_boolean(config, :agent_singleton), '', set_as_integer(config, :agent_prefetch))
-      @agency = Struct::Agency.new(cache_path, config[:agent_path], acl_path, cache_path.join('acl'), config[:agency_pid_dir])
-      @amqp = Struct::Amqp.new(broker, amqp_opts, amqp_opts, Struct::Publish.new({}), Struct::Subscribe.new(true), Struct::Pop.new(true))
-      @eventmachine = Struct::Eventmachine.new(set_as_integer(config, :file_descriptors, 1024), set_as_boolean(config, :epoll, true), set_as_boolean(config, :kqueue, true))
-      @logging = Struct::Logging.new(config[:logging_trace], config[:logging_level], config[:logging_pattern], config[:logging_date_pattern], appender)
-      @smith = Struct::Smith.new(config[:smith_namespace], set_as_integer(config, :smith_timeout))
-
-      # Set the default ruby runtime. This will use the ruby that is in the path.
-      @ruby = Hash.new(config[:default_vm] || 'ruby')
-
-      config[:agent_vm] && config[:agent_vm].split(/\s+/).each do |vm_spec|
-        agent, vm = vm_spec.split(/:/)
-        @ruby[agent] = vm
-      end
-
-      find_config_file
+      toml = TOML.parse(read_config_file(find_config_file), :symbolize_keys => true)
+      @config = coerce_paths!(ConfigHash.new(default_amqp_opts).deep_merge(toml))
     end
 
     # Read the config file
     def read_config_file(config_file)
       @config_file = config_file
-      config_file.readlines.inject({}) do |a, line|
-        a.tap do |acc|
-          parameters = line.gsub(/#.*$/, '').strip
-          unless parameters.empty?
-            key, value = parameters.split(/\s+/, 2)
-            a[key.to_sym] = value
-          end
-        end
+      config_file.read
+    end
+
+    # Convert paths to Pathnames's
+    def coerce_paths!(config)
+      config.tap do |c|
+        c.agency[:pid_dir] = to_pathname(c.agency.pid_dir)
+        c.agency[:cache_path] = to_pathname(c.agency.cache_path)
+        c.agency[:agent_path] = to_pathname(c.agency.agent_path)
+        c.agency[:acl_path] = to_pathname(c.agency.acl_path)
       end
     end
 
@@ -165,6 +97,21 @@ module Smith
         end
         find_config_file(path.dirname)
       end
+    end
+
+    def default_amqp_opts
+      { :amqp => {
+          :exchange => {:durable => true, :auto_delete => false},
+          :queue => {:durable => true, :auto_delete => false},
+          :pop => {:ack => true},
+          :publish => {:headers => {}},
+          :subscribe => {:ack => true}
+        }
+      }
+    end
+
+    def to_pathname(p)
+      Pathname.new(p).expand_path
     end
   end
 end
--- spec/lib/config_spec.rb
+++ spec/lib/config_spec.rb
@@ -1,18 +1,67 @@
 # -*- encoding: utf-8 -*-
 
 require 'spec_helper'
+require 'config'
 
 describe Smith::Config do
 
-  before(:each) do
-    @config = Smith::Config.get
+  let(:config) { Smith::Config.get }
+
+  it "raise an exception if the config can't be found" do
+    expect do
+      Smith::Config.get("unknown-file")
+    end.to raise_error(Smith::ConfigNotFoundError)
   end
 
-  it 'should set meaning defaults' do
-    @config.eventmachine.file_descriptors.should == 1024
-    @config.amqp.publish.ack.should == true
-    @config.amqp.exchange.durable.should == true
+  it 'agent' do
+    agent = config.agent
+    expect(config.agent.monitor).to eq(false)
+    expect(config.agent.singleton).to eq(true)
+    expect(config.agent.metadata).to eq("")
+    expect(config.agent.prefetch).to eq(2)
+  end
 
-    @config.smith.namespace.should == 'smith'
+  it 'smith' do
+    expect(config.smith.namespace).to eq('smith')
+    expect(config.smith.timeout).to eq(4)
+  end
+
+  it 'eventmachine' do
+    expect(config.eventmachine.epoll).to eq(true)
+    expect(config.eventmachine.kqueue).to eq(false)
+    expect(config.eventmachine.file_descriptors).to eq(1024)
+  end
+
+  it 'amqp' do
+    expect(config.amqp.exchange.durable).to eq(true)
+    expect(config.amqp.exchange).to eq({:durable => true, :auto_delete => false})
+    expect(config.amqp.queue).to eq(:durable => true, :auto_delete => false)
+    expect(config.amqp.publish).to eq(:headers => {})
+    expect(config.amqp.subscribe).to eq(:ack => true)
+    expect(config.amqp.pop).to eq({:ack => true})
+  end
+
+  it 'vm' do
+    expect(config.vm.agent_default).to eq('/usr/local/ruby-2.1.0/bin/ruby')
+    expect(config.vm.NullAgent).to eq('/usr/local/ruby-2.1.1/bin/ruby')
+  end
+
+  it 'agency' do
+    expect(config.agency.pid_dir).to eq(Pathname.new("/run/smith"))
+    expect(config.agency.cache_path).to eq(Pathname.new("/var/cache/smith/lmdb"))
+    expect(config.agency.agent_path).to eq(Pathname.new("/home/rgh/dev/ruby/smith2/agents"))
+    expect(config.agency.acl_path).to eq(Pathname.new("/home/rgh/dev/ruby/smith2/lib/acl"))
+  end
+
+  it 'logging' do
+    expect(config.logging.trace).to eq(true)
+    expect(config.logging.level).to eq('verbose')
+  end
+
+  it 'appender' do
+    expect(config.appender.filename).to eq("/var/log/smith/smith.log")
+    expect(config.appender.type).to eq("RollingFile")
+    expect(config.appender.age).to eq("daily")
+    expect(config.appender.keep).to eq(100)
   end
 end
