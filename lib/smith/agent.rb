9e7da250a057c39943be5dcbc077f43bd4e27d1f
Richard Heycock <rgh@filterfish.org>
2014-09-27 18:11:43 +1000

    Initial implementation of the self-pipe trick.

--- lib/smith/agent.rb
+++ lib/smith/agent.rb
@@ -15,8 +15,7 @@ module Smith
 
       @factory = QueueFactory.new
 
-      @signal_handlers = Hash.new { |h,k| h[k] = Array.new }
-
+      setup_signal_handlers
       setup_control_queue
 
       @start_time = Time.now
@@ -73,14 +72,46 @@ module Smith
       raise ArgumentError, "You must override this method"
     end
 
+    # Set up the mechanics required to implement the self pipe trick.
+    def setup_signal_handlers
+      @signal_handlers = Hash.new { |h,k| h[k] = Array.new }
+      @signal_handler_queue = []
+      @signal_handler_pipe_reader, @signal_handler_pipe_writer = IO.pipe
+
+      # The Module declaration here will only close over local variables so we
+      # need to assign self to a local variable to get access to the agent itself.
+      agent = self
+
+      EM.attach(@signal_handler_pipe_reader, Module.new {
+        define_method :receive_data do |_|
+
+          handlers = agent.instance_variable_get(:@signal_handlers)
+          queue = agent.instance_variable_get(:@signal_handler_queue)
+          signal = queue.pop
+
+          agent.send(:logger).debug { "Running signal handlers for agent: #{agent.name}: #{signal}" }
+          handlers[signal].each { |handler| handler.call(signal) }
+        end
+      })
+    end
+
+    # Adds a signal handler proc to the list of signal_handlers. When
+    # a signal is received all signal handlers are called in reverse order.
+    # @param signal [Integer] the signal number.
+    # @param position [Symbol] [:first|:last] where on the signal handler stack
+    #                          to put the handler proc.
+    # @see Signal.list for a full list of available signals on a system.
     def install_signal_handler(signal, position=:end, &blk)
       raise ArgumentError, "Unknown position: #{position}" if ![:beginning, :end].include?(position)
+      logger.debug { "Installing signal handler for #{signal}" }
 
-      logger.verbose { "Installing signal handler for #{signal}" }
       @signal_handlers[signal].insert((position == :beginning) ? 0 : -1, blk)
-      @signal_handlers.each do |sig, handlers|
-        trap(sig, proc { |sig| run_signal_handlers(sig, handlers) })
-      end
+
+      Signal.trap(signal) {
+        logger.debug { "Got signal: #{signal}" }
+        @signal_handler_pipe_writer.write_nonblock('.')
+        @signal_handler_queue << signal
+      }
     end
 
     def state
@@ -112,12 +143,7 @@ module Smith
       end
     end
 
-    protected
-
-    def run_signal_handlers(sig, handlers)
-      logger.debug { "Running signal handlers for agent: #{name}: #{sig}" }
-      handlers.each { |handler| handler.call(sig) }
-    end
+    private
 
     def setup_control_queue
       logger.debug { "Setting up control queue: #{control_queue_def.denormalise}" }
