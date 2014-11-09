fe0aa7ba92c827964bdee54c54cf8be4d1aa982e
Richard Heycock <rgh@filterfish.org>
2014-11-09 12:35:36 +1100

    Improve error handling and reporting.
    There are case where queues or exchanges don't get cleaned up properly
    so when you try and remove them an error occurs and the command aborts
    make it painfull if you need to remove large numbers of queues. This
    adds two new options: --ignore-errors --log-errors to override this
    behaviour.
    
    I've also replaced the --verbose option with a FIXME as it needs to be
    done properly.

--- lib/smith/commands/smithctl/rm.rb
+++ lib/smith/commands/smithctl/rm.rb
@@ -7,24 +7,16 @@ module Smith
         when 0
           responder.succeed("No queue specified. Please specify a queue.")
         else
-          @on_error = proc do |ch,channel_close|
-            case channel_close.reply_code
-            when 404
-              responder.succeed("No such queue: [#{channel_close.reply_code}]: #{channel_close.reply_text}")
-            when 406
-              responder.succeed("Queue not empty: [#{channel_close.reply_code}]: #{channel_close.reply_text}.")
-            else
-              responder.succeed("Unknown error: [#{channel_close.reply_code}]: #{channel_close.reply_text}")
+          worker = ->(queue_name, iter) do
+            delete_queue(queue_name) do |delete_ok|
+              delete_exchange(queue_name, &iter)
             end
           end
 
-          target.each do |queue_name|
-            delete_queue(queue_name) do |delete_ok|
-              delete_exchange(queue_name) do |delete_ok|
-                responder.succeed((options[:verbose]) ? delete_ok.message_count.to_s : nil)
-              end
-            end
-          end
+          # FIXME: Return errors to the caller rather than doing nothing or logging errors
+          done = -> { responder.succeed }
+
+          EM::Iterator.new(target).each(worker, done)
         end
       end
 
@@ -33,14 +25,26 @@ module Smith
       def options_spec
         banner "Display or remove a message from the named queue."
 
-        opt    :force,   "force the removal even if there are messages on the queue", :short => :f
-        opt    :verbose, "print the number of messages deleted", :short => :v
+        opt :force,         "force the removal even if there are messages on the queue", :short => :f
+        opt :ignore_errors, "ignore any errors.", :default => false
+        opt :log_errors,  "print any errors messages.", :default => false
       end
 
-      def delete_exchange(exchange_name, &blk)
-        AMQP::Channel.new(Smith.connection) do |channel,ok|
-          channel.on_error(&@on_error)
-          channel.direct("smith.#{exchange_name}", :passive => true) do |exchange|
+      # Delete an exchange.
+      #
+      # @param name [String] name of the exchange.
+      # @yield calls the block when the exchange has been deleted
+      # @yieldparam [AMQ::Protocol::Channel::Close] the amqp close message
+      # FIXME: remove duplication
+      def delete_exchange(name, &blk)
+        AMQP::Channel.new(Smith.connection) do |channel, ok|
+
+          channel.on_error do |channel, channel_close|
+            handler = (options[:ignore_errors]) ? blk : nil
+            log_error(channel, channel_close, &handler)
+          end
+
+          channel.direct("smith.#{name}", :passive => true) do |exchange|
             exchange_options = (options[:force]) ? {} : {:if_unused => true}
             exchange.delete(exchange_options) do |delete_ok|
               blk.call(delete_ok)
@@ -49,15 +53,40 @@ module Smith
         end
       end
 
+      # Delete an queue.
+      #
+      # @param name [String] name of the queue.
+      # @yield calls the block when the queue has been deleted
+      # @yieldparam [AMQ::Protocol::Channel::Close] the amqp close message
+      # FIXME: remove duplication
       def delete_queue(queue_name, &blk)
-        AMQP::Channel.new(Smith.connection) do |channel,ok|
-          channel.on_error(&@on_error)
+        AMQP::Channel.new(Smith.connection) do |channel, ok|
+          channel.on_error do |channel, channel_close|
+            handler = (options[:ignore_errors]) ? blk : nil
+            log_error(channel, channel_close, &handler)
+          end
+
           channel.queue("smith.#{queue_name}", :passive => true) do |queue|
             queue_options = (options[:force]) ? {} : {:if_unused => true, :if_empty => true}
             queue.delete(queue_options) do |delete_ok|
               blk.call(delete_ok)
             end
           end
+        end
+      end
+
+      # Get's called when there is a channel error.
+      #
+      # @param channel [AMQP::Channel] the channel that errored
+      # @param channel_close [AMQ::Protocol::Channel::Close] the amqp close message
+      #        which contains details of why the channel was claosed.
+      def log_error(channel, channel_close, &blk)
+        base_error_msg = "#{channel_close.reply_code}, #{channel_close.reply_text}."
+        if blk
+          logger.error { "#{base_error_msg}. Ignoring as requested" } if options[:log_errors]
+          blk.call
+        else
+          responder.succeed(base_error_msg)
         end
       end
 
