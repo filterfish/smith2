cff05c74079cf82beed04d3e5ff964b28d402c1a
Richard Heycock <rgh@filterfish.org>
2014-11-09 12:59:14 +1100

    Simple command to list the agents in a group.

---
+++ lib/smith/commands/agency/group.rb
@@ -0,0 +1,29 @@
+require_relative '../common'
+
+module Smith
+  module Commands
+    class Group < CommandBase
+
+      include Common
+
+      def execute
+        group do |value|
+          responder.succeed(value)
+        end
+      end
+
+      # Returns the agents in a group.
+      def group(&blk)
+        begin
+          blk.call(agent_group(target.first).join(' '))
+        rescue RuntimeError => e
+          blk.call(e.message)
+        end
+      end
+
+      def options_spec
+        banner "Lists the agents in a group."
+      end
+    end
+  end
+end
