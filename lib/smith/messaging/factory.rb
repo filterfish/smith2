29fd34f834e9f02c9fe0d0711538c61b1df343b2
Richard Heycock <rgh@filterfish.org>
2014-11-09 12:10:08 +1100

    extend Utils instead of include'ing. Fix method name.

--- lib/smith/messaging/factory.rb
+++ lib/smith/messaging/factory.rb
@@ -1,17 +1,18 @@
 # -*- encoding: utf-8 -*-
-module Smith
 
+module Smith
   module ACL
     class Factory
       include Logger
-      include Utils
+
+      extend Utils
 
       class << self
         def create(type, content=nil, &blk)
           if type.respond_to?(:serialize_to_string)
             return type
           else
-            clazz = (type.is_a?(::Protobuf::Message)) ? type : get_clazz(type)
+            clazz = (type.is_a?(::Protobuf::Message)) ? type : class_from_name(type)
 
             if blk
               clazz.new.tap { |m| blk.call(m) }
