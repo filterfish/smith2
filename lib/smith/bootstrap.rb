f2d5c013a1cbfcf3d38de7db7ce89f97e5ccfbd7
Dylan Griffith <dyl.griffith@gmail.com>
2014-12-22 17:01:34 +1100

    add ability to hook into exceptions

--- lib/smith/bootstrap.rb
+++ lib/smith/bootstrap.rb
@@ -65,7 +65,7 @@ module Smith
     # be running. So it must be restarted and then shutdown again
     # See the note at the in main.
     def terminate!(exception=nil)
-      @agent.on_exception.call(exception)
+      @agent.exception_handler.call(exception)
       logger.error { format_exception(exception) } if exception
       logger.error { "Terminating: #{@agent_uuid}." }
 
