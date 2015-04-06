da77da8b13d6cc3280e4e616421a2e034421acfa
Richard Heycock <rgh@filterfish.org>
2015-04-06 15:24:11 +1000

    Update NullAgent to use a method instead of a block

--- agents/null_agent.rb
+++ agents/null_agent.rb
@@ -6,11 +6,12 @@ class NullAgent < Smith::Agent
   options :metadata => "Some sage words about the NullAgent."
 
   def run
-    receiver('agent.null.queue', :auto_ack => false, :auto_delete => false) do |queue|
-      queue.subscribe do |payload, receiver|
-        logger.debug("Payload: #{payload.inspect.gsub(/\r|\n/n, ', ')}")
-        receiver.ack
-      end
-    end
+    receiver('agent.null.queue', :auto_ack => false, :auto_delete => false).subscribe(method(:worker))
   end
+
+  def worker(payload, receiver)
+    logger.debug("Payload: #{payload.inspect.gsub(/\r|\n/n, ', ')}")
+    receiver.ack
+  end
+
 end
