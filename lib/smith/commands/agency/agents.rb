1ff17166793137ea452b71acee84a441ff2dd5f8
Richard Heycock <rgh@filterfish.org>
2015-06-01 23:12:34 +1000

    Add group support to the agents command.

--- lib/smith/commands/agency/agents.rb
+++ lib/smith/commands/agency/agents.rb
@@ -1,7 +1,11 @@
 # -*- encoding: utf-8 -*-
+require_relative '../common'
+
 module Smith
   module Commands
     class Agents < CommandBase
+      include Common
+
       def execute
         responder.succeed(_agents)
       end
@@ -10,13 +14,17 @@ module Smith
       def _agents
         separator = (options[:one_column]) ? "\n" : " "
 
-        Smith.agent_directories.inject([]) do |acc, agent_root_path|
-          Pathname.glob(agent_root_path.join("**/*")).each do |agent_path|
-            if !agent_path.symlink? && !agent_path.directory?
-              acc << Utils.class_name_from_path(agent_path, agent_root_path)
+        if options[:group]
+          agent_group(options[:group]).sort.join(separator)
+        else
+          Smith.agent_directories.each_with_object([]) do |agent_root_path, acc|
+            Pathname.glob(agent_root_path.join("**/*")).each do |agent_path|
+              if !agent_path.symlink? && !agent_path.directory?
+                acc << Utils.class_name_from_path(agent_path, agent_root_path)
+              end
             end
-          end
-        end.sort.join(separator)
+          end.sort.join(separator)
+        end
       end
 
       private
@@ -25,6 +33,7 @@ module Smith
         banner "List all available agents."
 
         opt    :one_column, "List one agent per line", :short => :s
+        opt    :group,      "list only agents in this group", :type => :string, :short => :g
       end
     end
   end
